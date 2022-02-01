592359f14159aa6eb869c83640ed23db196c1fdc
hamza3533 <hamza.shaikh3533@gmail.com>
2022-02-01 17:57:12 +0500

    edit profile Screen

--- Component/PodcastsComponent.js
+++ Component/PodcastsComponent.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 export default function PodcastsComponent(props) {
-  
-  return <div className='brdr-2 col-lg-2 col-md-4 col-6 hover'>
-    <img src={props.Imgsrc} />
-    <div className='p-2'>
-      <p className='mb-0 fs-14 Bold'>{props.title}</p>
-      <p className='mb-0 txt-yellow'>{props.showBy}</p>
+
+  return <div className='brdr-2 col-lg-2 col-md-4 col-6 hover d-flex flex-column h-100'>
+    <div style={{ flex: "1 1 auto" }}>
+      <img src={props.Imgsrc} />
+      <div className='p-2'>
+        <p className='mb-0 fs-14 Bold'>{props.title}</p>
+        <p className='mb-0 txt-yellow'>{props.showBy}</p>
+      </div>
     </div>
   </div>;
 }
