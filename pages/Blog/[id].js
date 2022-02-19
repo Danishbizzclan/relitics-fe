61f40e4c02ea5fefbcc0eaf864b785248ede92d2
hamza3533 <hamza.shaikh3533@gmail.com>
2022-02-19 12:50:05 +0500

    nav Img CSS_

--- pages/Blog/[id].js
+++ pages/Blog/[id].js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import { useRouter } from "next/router";
-import { getEventById } from '../../Component/Data/BlogData'
 import Navbar from '../../Component/Navbar';
 import Foter from '../../Component/Footer';
 import Categories from '../../Component/Categories';
@@ -35,10 +34,10 @@ const BlogDetail = () => {
 
 
   if (loading) {
-    return(
+    return (
       <div className='text-center mt-5'>
-    <Spin />
-    </div>
+        <Spin />
+      </div>
     )
   }
 
@@ -94,39 +93,6 @@ const BlogDetail = () => {
                   <Categories />
                 </div>
               </div>
-
-              {/* <div className="accordion accordion-flush m-0 p-3" id="accordionFlushExample">
-                <div className="accordion-item">
-                  <h2 className="accordion-header" id="flush-headingOne">
-                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                      How to <span className='sec-color ms-2'>(73)</span>
-                    </button>
-                  </h2>
-                  <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                    <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
-                  </div>
-                </div>
-                <div className="accordion-item">
-                  <h2 className="accordion-header" id="flush-headingTwo">
-                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                      How to <span className='sec-color ms-2'>(73)</span>
-                    </button>
-                  </h2>
-                  <div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-                    <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item's accordion body. Let's imagine this being filled with some actual content.</div>
-                  </div>
-                </div>
-                <div className="accordion-item">
-                  <h2 className="accordion-header" id="flush-headingThree">
-                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-                      How to <span className='sec-color ms-2'>(73)</span>
-                    </button>
-                  </h2>
-                  <div id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-                    <div className="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the third item's accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.</div>
-                  </div>
-                </div>
-              </div> */}
             </div>
           </div>
         </div>
