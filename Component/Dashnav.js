bbec459b802d0658b544ef727ce1d7fdd877afeb
hamza3533 <hamza.shaikh3533@gmail.com>
2022-02-22 16:27:37 +0500

    abhi wala

--- Component/Dashnav.js
+++ Component/Dashnav.js
@@ -6,16 +6,33 @@ import Link from "next/link"
 function Dashnav() {
   const router = useRouter();
   const [user, setUser] = useState('')
+  const [notificationOPen, setNotificationOpen] = useState(false);
+  const [dashboardOpen, setDashboardOpen] = useState(false);
   useEffect(() => {
 
-  if (typeof window !== 'undefined') {
+    if (typeof window !== 'undefined') {
 
-    setUser(JSON.parse(localStorage.getItem('user')))
+      setUser(JSON.parse(localStorage.getItem('user')))
+    }
+  }, [typeof window])
+
+  const Logout = () => {
+    localStorage.clear();
+    window.location.href = '/';
   }
-},[typeof window])
-  
 
-
+  function notificationExpand() {
+    setNotificationOpen(true);
+  }
+  function notificationClose() {
+    setNotificationOpen(false);
+  }
+  function dashboardExpand() {
+    setDashboardOpen(true);
+  }
+  function dashboardClose() {
+    setDashboardOpen(false);
+  }
 
   return (
     <div className={classes.dashNavColor}>
@@ -28,8 +45,8 @@ function Dashnav() {
                   <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                  <ul className="navbar-nav ms-auto  mb-lg-0">
-                    <Link href="/" ><div className={`nav-link fs-15  ${classes.NavBtnPadding + ' ' + classes.navbarText } ${router.pathname == "/" ? "active" : null}`}>
+                  <ul className="navbar-nav ms-auto mb-lg-0">
+                    <Link href="/" ><div className={`nav-link fs-15  ${classes.NavBtnPadding + ' ' + classes.navbarText} ${router.pathname == "/" ? "active" : null}`}>
                       Home</div>
                     </Link>
                     <Link href="/#Prices" ><div className={`nav-link fs-15 ${classes.NavBtnPadding + ' ' + classes.navbarText} ${router.pathname == "/" ? "active" : null}`}>
@@ -40,61 +57,92 @@ function Dashnav() {
                       Articles</div></Link>
                     <Link href="/Contact" ><div className={`nav-link fs-15 ${classes.NavBtnPadding + ' ' + classes.navbarText} ${router.pathname == "/Contact" ? "active" : null}`}>
                       Contact</div></Link>
-                    <Link href="/Notifications">
-                      <img src={'/NotificationNav.svg'} className={`mx-3 text-lg-start noti-w ${classes.notiBtn}`} style={{ objectFit: 'contain' }} /></Link>
-                    <Link href="/Dashboard">
-                      <div className={`d-flex rounded-pill name-bg my-sm-2 my-md-auto ${classes.profileBtn}`}>
-                        <img src={user?.image} className='rounded_img' style={{ width: '4.6rem' }} />
-                        <p className="text-white my-auto px-sm-0 px-md-5 fs-15">{user?.username}</p>
-                      </div>
-                    </Link>
+                    <details className="dropdown text-white my-auto">
+                      <summary role="button">
+                        <span className="button">
+                          <img src={'/NotificationNav.svg'} className={`mx-3 text-lg-start noti-w ${classes.notiBtn}`} style={{ objectFit: 'contain' }} />
+                        </span>
+                      </summary>
+                      <ul>
+                        <li>
+                          <div className='d-inline-flex w-100 px-3 brdr_btm'>
+                            <img src='./notificationImg.png' style={{ width: "20%" }} alt='img' />
+                            <p className='my-auto fs-14 light_font ms-3'>ksajhdi soakf sjka kawjdsklaj akjdn s</p>
+                          </div>
+                        </li>
+                        <li>
+                          <div className='d-inline-flex w-100 px-3 brdr_btm'>
+                            <img src='./notificationImg.png' style={{ width: "20%" }} alt='img' />
+                            <p className='my-auto fs-14 light_font ms-3'>ksajhdi soakf sjka kawjdsklaj akjdn s</p>
+                          </div>
+                        </li>
+                        <li>
+                          <div className='d-inline-flex w-100 px-3 brdr_btm'>
+                            <img src='./notificationImg.png' style={{ width: "20%" }} alt='img' />
+                            <p className='my-auto fs-14 light_font ms-3'>ksajhdi soakf sjka kawjdsklaj akjdn s</p>
+                          </div>
+                        </li>
+                        <li>
+                          <div className='d-inline-flex w-100 px-3 brdr_btm'>
+                            <img src='./notificationImg.png' style={{ width: "20%" }} alt='img' />
+                            <p className='my-auto fs-14 light_font ms-3'>ksajhdi soakf sjka kawjdsklaj akjdn s</p>
+                          </div>
+                        </li>
+                        <Link href="/Notifications">
+                          <div className='fs-15 text-center bluetxt'>show full notifications</div>
+                        </Link>
+                      </ul>
+                    </details>
+
+                    <details className="dropdown text-white my-auto">
+                      <summary role="button">
+                        <span className={`d-flex button rounded-pill name-bg my-sm-2 my-md-auto ${classes.profileBtn}`}>
+                          <img src={user?.image} className='rounded_img' style={{ width: '4.6rem' }} />
+                          <p className="text-white my-auto px-sm-0 ps-md-3 pe-md-5 fs-15">{user?.username}</p>
+                        </span>
+                      </summary>
+                      <ul>
+                        <li>
+                          <Link href="/Dashboard" >
+                            <div className="d-inline-flex w-100  px-3 py-4 brdr_btm">
+                              <img src={"/editLogo_black.svg"} className='mx-3 my-auto' /><p className='fs-14 ms-3 mb-0'>My Dashboard</p>
+                            </div></Link>
+                        </li>
+                        <li>
+                          <Link href="/EditProfile" >
+                            <div className="d-inline-flex w-100  px-3 py-4 brdr_btm">
+                              <img src={"/editLogo_black.svg"} className='mx-3 my-auto' /><p className='fs-14 ms-3 mb-0'>Edit Profile</p>
+                            </div></Link>
+                        </li>
+                        <li>
+                          <Link href="/MyFavourites" >
+                            <div className="d-inline-flex w-100  px-3 py-4 brdr_btm">
+                              <img src={"/heart_black.svg"} className='mx-3 my-auto' /><p className='fs-14 ms-3 mb-0'>My Favourites</p>
+                            </div></Link>
+                        </li>
+                        <li>
+                          <Link href="/Notifications" >
+                            <div className="d-inline-flex w-100  px-3 py-4 brdr_btm">
+                              <img src={"/bell_black.svg"} className='mx-3 my-auto' /><p className='fs-14 ms-3 mb-0'>Notifications</p>
+                            </div></Link>
+                        </li>
+                        <li>
+                          <Link href="/Blog" >
+                            <div className="d-inline-flex w-100  px-3 py-4 brdr_btm"  onClick={Logout}>
+                              <img src={"/Logout_black.svg"} className='mx-3 my-auto' /><p className='fs-14 ms-3 mb-0'>Log Out</p>
+                            </div></Link>
+                        </li>
+                      </ul>
+                    </details>
                   </ul>
                   {/* <img className="p-2 rounded-pill" src="Path 188.png" alt="" /> */}
                 </div>
               </div>
             </nav>
           </div>
-        </div>
+        </div >
       </div >
     </div >
   )
 }
 export default Dashnav
-
-    // <>
-    //   {/* <div className='dash-nav '>
-    //     <div className="container py-4">
-    //           <nav className="navbar navbar-expand-lg p-0 navbar-light ">
-    //             <div className="container-fluid p-0">
-    //               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    //                 <span className="navbar-toggler-icon"></span>
-    //               </button>
-    //               <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    //                 <ul className="navbar-nav ms-auto mb-lg-0">
-    //                   <Link href="/" className="nav-item">
-    //                     <div className={`nav-link navbar-text text-lg-start nav-bor fs-15 p-0 my-auto mx-3 dash-text`} aria-current="page">Home</div>
-    //                   </Link>
-    //                   <Link href="/BecomeProfessional" className="nav-item">
-    //                     <div className={`nav-link navbar-text nav-bor text-lg-start fs-15 p-0 my-auto mx-3 dash-text`} aria-current="page">Pricing</div>
-    //                   </Link>
-    //                   <Link href="/About" className="nav-item">
-    //                     <div className={`nav-link navbar-text nav-bor text-lg-start fs-15 p-0 my-auto mx-3  dash-text ${router.pathname == "/About" ? "active" : null}`} aria-current="page">About</div>
-    //                   </Link>
-    //                   <Link href="/Blog" className="nav-item">
-    //                     <div className={`nav-link navbar-text fs-15 p-0 my-auto text-lg-start mx-3 nav-bor nav-bordash-text dash-text`} aria-current="page">Article</div>
-    //                   </Link>
-    //                   <Link href="/Contact" className="nav-item">
-    //                     <div className={`nav-link navbar-text fs-15 p-0 my-auto mx-3 text-lg-start nav-bor  dash-text`} aria-current="page">contact</div>
-    //                   </Link>
-    //                   <img src={'/NotificationNav.svg'} className="mx-3 text-lg-start noti-w" style={{ objectFit: 'contain' }} />
-    //                   <div className={`d-flex rounded-pill name-bg my-sm-2 my-md-auto`}>
-    //                     <img src={'/Mask Group 23.png'} style={{ width: '4.6rem' }} />
-    //                     <p className="text-white my-auto px-sm-0 px-md-5 fs-15">Tabish</p>
-    //                   </div>
-    //                 </ul>
-    //               </div>
-    //             </div>
-    //           </nav>
-    //         </div>
-    //   </div> */}
-    // </>
